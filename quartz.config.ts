d5bad7829374b3998e3f60fce2f7acd4d62f938d
xiaoguan521 <xiaochen1649@gmail.com>
2026-04-10 15:18:43 +0800

    走你

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.sy110.nyc.mn",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
